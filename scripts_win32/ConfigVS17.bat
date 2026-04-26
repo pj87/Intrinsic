2b46e8d5e4e724c402dd95749e086027e30ec1f0
Paweł Jastrzębski <p.k.jastrzebski@gmail.com>
2026-04-26 17:46:25 +0200

    Add builds for MSVC 2022 and MSVC 2026
    Added configurations scripts for:
    - MSVC 17 2022
    - MSVC 18 2026
    Increased minimum CMake version to align with CMake 4.3.2 to support generator for MSVC 2026.
    Fixed compilation errors which occured for dependencies:
    - SOL
    - gli
    - lua
    - STL: chrono
    
    Signed-off-by: Paweł Jastrzębski <p.k.jastrzebski@gmail.com>

---
+++ scripts_win32/ConfigVS17.bat
@@ -0,0 +1,7 @@
+cd ..
+mkdir build
+cd build
+
+cmake -G"Visual Studio 17 2022" -A x64 ..
+
+cd ..\scripts_win32
---
+++ scripts_win32/ConfigVS18.bat
@@ -0,0 +1,7 @@
+cd ..
+mkdir build
+cd build
+
+cmake -G"Visual Studio 18 2026" -A x64 ..
+
+cd ..\scripts_win32
