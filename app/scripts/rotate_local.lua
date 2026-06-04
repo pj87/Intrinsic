615b555ff99969b90500fb1a4a351a49fd36f5bd
Paweł Jastrzębski <p.k.jastrzebski@gmail.com>
2026-06-04 17:18:33 +0200

    Slow down Mandelbulb rotation and reverse direction (-0.2 rad/s)
    Signed-off-by: Paweł Jastrzębski <p.k.jastrzebski@gmail.com>

--- app/scripts/rotate_local.lua
+++ app/scripts/rotate_local.lua
@@ -1,7 +1,7 @@
 function tick(p_EntityRef, p_DeltaT)
   local nodeRef = nodeComponent.getComponentForEntity(p_EntityRef)
 
-  local rotation = Quat.new(Vec3.new(0.0, p_DeltaT * 0.5, 0.0))
+  local rotation = Quat.new(Vec3.new(0.0, p_DeltaT * -0.2, 0.0))
   local orientation = nodeComponent.getOrientation(nodeRef)
   nodeComponent.setOrientation(nodeRef, glm.rotate(orientation, rotation))
 
